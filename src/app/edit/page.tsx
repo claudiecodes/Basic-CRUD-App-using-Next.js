d4bca6259e815e250c4ecee3d340e58bd32f54af
claudiecodes <193557157+claudiecodes@users.noreply.github.com>
2025-01-16 01:28:37 +0700

    fix: deployment error

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,9 +1,12 @@
 import EditFormComp from "@/components/EditFormComp";
+import { Suspense } from "react";
 
 export default function EditPage() {
   return (
     <>
-      <EditFormComp />
+      <Suspense fallback={<div>Loading...</div>}>
+        <EditFormComp />
+      </Suspense>
     </>
   );
 }
