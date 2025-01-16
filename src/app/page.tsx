48cbf49797428a475d5c061099146d85b781ce0c
claudiecodes <193557157+claudiecodes@users.noreply.github.com>
2025-01-16 11:30:05 +0700

    feat: theme toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use server";
 
 import DeleteBtnComp from "@/components/DeleteBtnComp";
+import { ThemeToggle } from "@/components/ThemeToggle";
+
 import { fetchGET } from "@/libs/actions";
 import { Note } from "@/libs/types";
 import Link from "next/link";
@@ -10,27 +12,38 @@ export default async function Home() {
 
   return (
     <>
-      <div>
-        {notes.map((note) => (
-          <div key={note._id}>
-            <div>Title:{note.title}</div>
-            <div>Description: {note.description}</div>
-            <DeleteBtnComp _id={note._id} />
-            <div>
-              <Link
-                href={`/edit?id=${note._id}&title=${encodeURIComponent(
-                  note.title
-                )}&description=${encodeURIComponent(note.description)}`}
-              >
-                Edit
-              </Link>
-            </div>
+      <main className="p-10 justify-center">
+        <div className="flex justify-end mr-10">
+          <ThemeToggle />
+        </div>
+        <div className="flex justify-center font-bold p-10 text-3xl">
+          <span>Notes App</span>
+        </div>
+
+        <div className="flex justify-center p-10">
+          <div>
+            {notes.map((note) => (
+              <div key={note._id}>
+                <div>Title:{note.title}</div>
+                <div>Description: {note.description}</div>
+                <DeleteBtnComp _id={note._id} />
+                <div>
+                  <Link
+                    href={`/edit?id=${note._id}&title=${encodeURIComponent(
+                      note.title
+                    )}&description=${encodeURIComponent(note.description)}`}
+                  >
+                    Edit
+                  </Link>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div>
-        <Link href="/add">Add new note</Link>
-      </div>
+          <div>
+            <Link href="/add">Add new note</Link>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
